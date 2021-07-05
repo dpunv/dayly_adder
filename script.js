edbe2e977c8c0bcb42bc77dcfe9750e8a4bb47a3
dpunv <39238291+dpunv@users.noreply.github.com>
2021-07-05 12:47:34 +0200

    fixes and some changes on delete method
    changes on delete method
    
    changed title

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var del=0;
 function st(){
     if(typeof(Storage) !== "undefined") {
         var key;
@@ -6,10 +7,12 @@ function st(){
             if (!localStorage.hasOwnProperty(key)) continue;
             s = s + "<button onclick=\"dt('" + key + "')\">" + localStorage.getItem(key) +"</button>";
         }
+        /*
         if(s == ""){
             localStorage.setItem("0", "hello world");
             s = s + "<button onclick=\"dt(0)\">" + localStorage.getItem("0") +"</button>";
         }
+        */
         document.getElementById("ml").innerHTML = s;
     } else {
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
@@ -23,8 +26,10 @@ function add(){
 }
 
 function dt(key){
-    localStorage.removeItem(key);
-    st()
+    if(del == 1){
+        localStorage.removeItem(key);
+        st()
+    }
 }
 
 function da(){
@@ -35,8 +40,23 @@ function da(){
     st()
 }
 
-window.onload = st;
+function delact(){
+    if(del == 0){
+        del = 1;
+        document.getElementById("delact").innerHTML = "add";
+        document.getElementById("da").style.display = "block";
+        document.getElementById("add").style.display = "none";
+    }
+    else{
+        del = 0
+        document.getElementById("delact").innerHTML = "delete";
+        document.getElementById("da").style.display = "none";
+        document.getElementById("add").style.display = "block";
+    }
+}
 
+window.onload = st;
+/*
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js').then( (registration) => {
@@ -46,3 +66,4 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+*/
